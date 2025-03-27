0b556ff5f80b8a2e78978373bcfec20ad494599a
Leonard <leonard@posteo.net>
2025-03-27 13:49:21 +0100

    Added Constants file

---
+++ ConnectFour/Constants.cs
@@ -0,0 +1,7 @@
+namespace ConnectFour {
+    public static class Constants {
+        public const int NumberOfRows = 6;
+        public const int NumberOfColumns = 7;
+        public const int TokensInLineForAWin = 4;
+    }
+}
